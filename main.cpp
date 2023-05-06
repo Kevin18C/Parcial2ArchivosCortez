4e60299651f73a53b801415366fd41a1691f5489
Kevin Cortéz <kcortezm@miumg.edu.gt>
2023-05-06 10:48:37 -0600

    Se agrego la funcionalidad del segundo ejercicio

--- main.cpp
+++ main.cpp
@@ -16,11 +16,18 @@ int main() {
     cout << "Ingrese la cantidad de archivos a crear: ";
     cin >> cantidad_archivos;
 
+    cout<<"Agregue el texto que aparecera en el documento:"<<endl;
+    cin.ignore();
+    getline(cin, texto);
+
     if (cantidad_archivos % 2 == 0) {
         for (int i = 1; i <= cantidad_archivos; i++) {
 
             ofstream archivo;
             archivo.open("C:\\Users\\kevin\\Documents\\Par\\" + nombre + to_string(i) + ".txt");
+            for (int j = 1; j <= i; j++) {
+                archivo << texto << endl;
+            }
             archivo.close();
         }
         Sleep(1000);
@@ -30,6 +37,9 @@ int main() {
 
             ofstream archivo;
             archivo.open("C:\\Users\\kevin\\Desktop\\impar\\" + nombre + to_string(i) + ".txt");
+            for (int j = 1; j <= i; j++) {
+                archivo << texto << endl;
+            }
             archivo.close();
         }
         Sleep(1000);
